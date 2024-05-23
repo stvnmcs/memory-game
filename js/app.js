f540c80e4a824b70e90bfcabbd5ef643277a6387
StevenMacias <stevenmcs22@gmail.com>
2024-05-23 10:14:36 -0400

    this is hard fr

--- js/app.js
+++ js/app.js
@@ -11,8 +11,35 @@ function turnoverTile() {
     if (!isTileTurnedover) {
         isTileTurnedover = true;
         firstTile = this;
+        return;
     }
+
+    secondTile = this;
+    isTileTurnedover = false;
+
+    doCharactersMatch();
   }
-  
+
+  function doCharactersMatch() {
+    if (firstTile.dataset.tile === secondTile.dataset.tile) {
+        turnTilesOff();
+        return;
+    }
+
+    revertTiles();
+  }
+
+  function turnTilesOff (){
+    firstTile.removeEventListener('click', turnoverTile);
+    secondTile.removeEventListener('click', turnoverTile);
+  }
+
+  function revertTiles(){
+    setTimeout(() => {
+        firstTile.classList.remove('turnover');
+        secondCard.classList.remove('turnover');
+    }, 1000);
+  }
+
 mkTiles.forEach(mkTile => mkTile.addEventListener('click', turnoverTile));
 
